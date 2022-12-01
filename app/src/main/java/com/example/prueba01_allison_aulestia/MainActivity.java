2568d9d83e7d83afa2e3cc73fed1e6df75cb905e
Allison Aulestia <aaulestia3784@edu.ec>
2022-12-01 18:49:46 -0500

    b

--- app/src/main/java/com/example/prueba01_allison_aulestia/MainActivity.java
+++ app/src/main/java/com/example/prueba01_allison_aulestia/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.prueba01_allison_aulestia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextNombre;
@@ -13,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        editTextNombre = findViewById(R.id.editTextNombre);
+        editTextApellido = findViewById(R.id.editTextApellido);
     }
+
+    @Override
+    public void NoEditable(View view){
+
+    }
+
 }
