47690b1a4b1a2e1a2fac86ee3b59256f5d69ef86
Allison Aulestia <aaulestia3784@edu.ec>
2022-12-01 19:10:50 -0500

    b

--- app/src/main/java/com/example/prueba01_allison_aulestia/MainActivity.java
+++ app/src/main/java/com/example/prueba01_allison_aulestia/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editTextNombre;
     private EditText editTextApellido;
+    private Button button_Siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
         editTextNombre = findViewById(R.id.editTextNombre);
         editTextApellido = findViewById(R.id.editTextApellido);
-    }
+        button_Siguiente = (Button) findViewById(R.id.button_Siguiente);
 
-    @Override
-    public void NoEditable(View view){
-
+        button_Siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent siguiente = new Intent(MainActivity.this, SecondActivity.class);
+               startActivity(siguiente);
+            }
+        });
     }
 
 }
